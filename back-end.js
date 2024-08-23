3b2eaedab2f251ffc4a70742488afc2d712e2d2b
kkingston32 <126709063+kkingston32@users.noreply.github.com>
2024-08-22 21:48:22 -0600

    .

--- back-end.js
+++ back-end.js
@@ -17,7 +17,7 @@ const secret_key = 'Iliketoeatapplesandbananas';
 // const origin = "https://thinkwellness.azurewebsites.net"
 
 // Middleware setup
-app.use(cors({ origin: 'https://thinkwellness.azurewebsites.net', credentials: true }));
+app.use(cors({ origin: 'https://thinkwellness.azurewebsites.net', credentials: true,  allowedHeaders: ['Content-Type', 'Authorization'], }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
